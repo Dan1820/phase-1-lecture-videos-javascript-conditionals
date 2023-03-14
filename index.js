7006774a44387f629f864a77130da99aefa2eedd
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-14 18:50:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+// function isRose(cat){
+//     return cat==='rose'?'Hi rose you are cute' : `hi ${cat} yo are not cute`
+// //     if(cat==="rose"){
+// //         return "hi rose your are cute"
+
+// //     }else if(typeof cat==='string'){
+// //         return`hi ${cat} you are not beautiful`
+// //     } else{
+// //         return 'hi you are nt a cat'
+// //     }
+    
+// }
+// console.log(isRose("pussy"));
+function uuRose(paket){
+    switch (paket){
+        case 'rose':
+            return 'Hi rose you are cute'
+        case 'ted': 
+        return 'hi ted ikarara wee pusit'
+        default:
+            return `hi ${paket} makimochin`    
+
+    }
+
+
+}
+console.log(uuRose('Ted'))
